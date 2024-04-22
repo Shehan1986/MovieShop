d63477f9a7dc775943a3fa57d4e3ca80ed2bcd89
Student <Student@DESKTOP-HP-PROD>
2024-04-22 13:26:56 +0200

    Added properties to Movie Class.

--- MovieShop_Oregano/Models/Movie.cs
+++ MovieShop_Oregano/Models/Movie.cs
@@ -1,6 +1,32 @@
-﻿namespace MovieShop_Oregano.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop_Oregano.Models
 {
     public class Movie
     {
+
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Movie Title")]
+        public string Title { get; set; }
+        
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public int ReleaseYear { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        
+        [DataType(DataType.ImageUrl)]
+        public string MovieImg { get; set; }
+
+        public virtual ICollection<OrderRow> OrderRows { get; set; } = new List<OrderRow>();
+
     }
 }
