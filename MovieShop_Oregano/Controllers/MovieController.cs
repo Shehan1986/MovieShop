799c127b99f1972b56ed0328e0b398d823e03480
Student <Student@DESKTOP-HP-PROD>
2024-04-22 13:35:01 +0200

    Created MovieController in Controllers.

---
+++ MovieShop_Oregano/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop_Oregano.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
