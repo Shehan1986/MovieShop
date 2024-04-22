c0bf71e9fe60ad8f4e908e160c7a3fbd3b90c9fc
Asa <asbo0900@student.miun.se>
2024-04-22 13:47:40 +0200

    Added code to Order and added OrderController.cs

--- MovieShop_Oregano/Models/Order.cs
+++ MovieShop_Oregano/Models/Order.cs
@@ -1,6 +1,18 @@
-﻿namespace MovieShop_Oregano.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop_Oregano.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy")]
+        public DateTime OrderDate { get; set; }
+
+        public virtual int CustomerId { get; set; }
+        public virtual ICollection<OrderRow> OrderRows { get; set; } = new List<OrderRow>();
+
     }
 }
