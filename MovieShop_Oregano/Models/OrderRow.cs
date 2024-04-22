a1956fde1ed4e68ded009681383bffd9ea483001
Student <Student@DESKTOP-HP-PROD>
2024-04-22 13:53:26 +0200

    added OrderRow

--- MovieShop_Oregano/Models/OrderRow.cs
+++ MovieShop_Oregano/Models/OrderRow.cs
@@ -1,6 +1,16 @@
-﻿namespace MovieShop_Oregano.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop_Oregano.Models
 {
     public class OrderRow
     {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int MovieId { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        public virtual Order Order { get; set; }
+        public virtual Movie Movie { get; set; }
     }
 }
