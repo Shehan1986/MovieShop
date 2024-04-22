5ff746e65f526a66ae84455a9938f6484cb604b3
Dell <Dell@DESKTOP-IB4DAQF>
2024-04-22 13:09:24 +0200

    Add project files.

---
+++ MovieShop_Oregano/Data/MovieDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieShop_Oregano.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ MovieShop_Oregano/Models/Movie.cs
@@ -0,0 +1,6 @@
+﻿namespace MovieShop_Oregano.Models
+{
+    public class Movie
+    {
+    }
+}
